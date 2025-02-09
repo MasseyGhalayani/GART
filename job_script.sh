f7205eeb5693495c687f8b00985e55069c104c04
mrrr120v <ms.ghalayani@gmail.com>
2025-02-09 20:23:54 +0100

    [update] job_script.sh to do all the fitting and eval.

--- job_script.sh
+++ job_script.sh
@@ -26,3 +26,45 @@ for seq in  "male-3-casual" "male-4-casual" "female-3-casual" "female-4-casual"
 do
     python solver.py --profile ./profiles/people/${profile}.yaml --dataset $dataset --seq $seq --eval_only --log_dir logs/${logbase}/seq=${seq}_prof=${profile}_data=${dataset}
 done
+
+# eval people
+for seq in  "male-3-casual" "male-4-casual" "female-3-casual" "female-4-casual"
+do
+    python solver.py --profile ./profiles/people/${profile}.yaml --dataset $dataset --seq $seq --eval_only --log_dir logs/${logbase}/seq=${seq}_prof=${profile}_data=${dataset}
+done
+
+
+dataset="instant_avatar_wild" # in ins-ava processing formart
+profile="ubc_mlp"
+logbase=${profile}
+
+SEQUENCES=(
+    "91+bCFG1jOS"
+    "91Iegdp9HFS"
+    "91fxYsir49S"
+    "91QFEra7jDS"
+    "91Ile3zLhMS"
+    "91WvLcNpdzS"
+)
+
+for seq in ${SEQUENCES[@]}; do
+    python solver.py --profile ./profiles/ubc/${profile}.yaml --dataset $dataset --seq ourpose_ubc_${seq} --logbase $logbase --fast --no_eval
+done
+
+
+dataset="instant_avatar_wild" # in ins-ava processing formart
+profile="ubc_mlp"
+logbase=${profile}
+
+SEQUENCES=(
+    "91+bCFG1jOS"
+    "91Iegdp9HFS"
+    "91fxYsir49S"
+    "91QFEra7jDS"
+    "91Ile3zLhMS"
+    "91WvLcNpdzS"
+)
+
+for seq in ${SEQUENCES[@]}; do
+    python solver.py --profile ./profiles/ubc/${profile}.yaml --dataset $dataset --seq ourpose_ubc_${seq} --eval_only --log_dir logs/${logbase}/seq=ourpose_ubc_${seq}_prof=${profile}_data=${dataset}
+done
